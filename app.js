3efa86d423847e4021ebeebeb5cf7ad7e36294fe
Kelechi Molokwu <kelechimolokwu@gmail.com>
2022-02-18 12:47:52 +0000

    Added Static and Logger Middleware || Updated package.json

--- app.js
+++ app.js
@@ -1,4 +1,34 @@
 const express = require("express")
 const cors = require("cors")
+const path = require("path")
+const fs = require("fs")
 const app = express()
+// const bodyParser = require('body-parser');
+const port = process.env.PORT || 3000
+
+// Middleware
+// Parser Middleware
+app.use(express.json());
+// app.use(bodyParser.json());
+
+// CORS allows you to configure the web API's security. 
+// Allows cross-domain API requests.
+app.use(cors())
+
+// Logger middleware
+app.use(function (req, res, next) {
+    console.log('Request IP: ' + req.url)
+    console.log('Request date: ' + new Date())
+    return next()
+  })
+
+// Static file middleware
+app.use(function (req, res, next) {
+    const filePath = path.join(__dirname, 'static', req.url)
+    fs.stat(filePath, function (err, fileInfo) {
+      if (err) return next()
+      if (fileInfo.isFile()) res.sendFile(filePath)
+      else next()
+    })
+  })
 
